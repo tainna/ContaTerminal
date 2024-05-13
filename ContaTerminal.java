40b8ab45b891251be4af82792c5aa85fd3ba92f5
tainna <64937004+tainna@users.noreply.github.com>
2024-05-13 16:03:48 -0300

    ContaTerminal.java
    Este programa Java permite que o usuário insira os dados da conta bancária via terminal e, em seguida, exibe uma mensagem com as informações fornecidas. Ele utiliza a classe Scanner para capturar a entrada do usuário a partir do terminal.

---
+++ ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibindo as informações da conta
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
+        // Fechar o scanner para liberar recursos
+        scanner.close();
+    }
+}
+
